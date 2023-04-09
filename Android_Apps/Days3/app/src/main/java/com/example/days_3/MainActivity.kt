b9476193018afef892cb370368fc255799da44b8
Hakan ÖZER <hakanozer02@gmail.com>
2023-04-09 21:01:29 +0300

    Intent

--- Android_Apps/Days3/app/src/main/java/com/example/days_3/MainActivity.kt
+++ Android_Apps/Days3/app/src/main/java/com/example/days_3/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.days_3
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var btnGotoProfile: Button
     lateinit var txtName: EditText
+    lateinit var txtId: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +24,24 @@ class MainActivity : AppCompatActivity() {
 
         btnGotoProfile = findViewById(R.id.btnGotoProfile)
         txtName = findViewById(R.id.txtName)
-        btnGotoProfile.setOnClickListener {
-            var intent = Intent(this, Profile::class.java)
-            intent.putExtra("name", "Erkan Bilsin")
-            intent.putExtra("id", 100)
+        txtId = findViewById(R.id.txtId)
 
-            user = UserData(txtName.text.toString(), "Bilsin", "serkan@mail.com", 150)
-            startActivity(intent)
-            // finish() mevcut sınıfı öldür
+        btnGotoProfile.setOnClickListener {
+            var stName = txtName.text.toString()
+            var stId = txtId.text.toString()
+            if ( stName.isNotEmpty() && stId.isNotEmpty()) {
+
+                var intent = Intent(this, Profile::class.java)
+                intent.putExtra("name", "Erkan Bilsin")
+                intent.putExtra("id", 100)
+
+                var id: Int = stId.toInt()
+                user = UserData(stName, "Bilsin", "serkan@mail.com", id)
+                startActivity(intent)
+                // finish() mevcut sınıfı öldür
+            }else {
+                Toast.makeText(this, "Input's Empty!", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }

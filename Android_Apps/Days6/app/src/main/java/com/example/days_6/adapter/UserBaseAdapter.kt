4ca8482993492d6b8fde514a32abb215e7e19b76
Hakan ÖZER <hakanozer02@gmail.com>
2023-04-16 23:13:40 +0300

    Days-7

---
+++ Android_Apps/Days6/app/src/main/java/com/example/days_6/adapter/UserBaseAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.days_6.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class UserBaseAdapter : BaseAdapter() {
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(p0: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+}
--- Android_Apps/Days6/app/src/main/java/com/example/days_6/adapter/UserCustomAdapter.kt
+++ Android_Apps/Days6/app/src/main/java/com/example/days_6/adapter/UserCustomAdapter.kt
@@ -4,7 +4,9 @@ import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.days_6.R
 import com.example.days_6.models.User
 
@@ -16,12 +18,15 @@ class UserCustomAdapter(private val context: Activity, private val list: List<Us
         val r_title = rootView.findViewById<TextView>(R.id.r_title)
         val r_email = rootView.findViewById<TextView>(R.id.r_email)
         val r_age = rootView.findViewById<TextView>(R.id.r_age)
+        val r_image = rootView.findViewById<ImageView>(R.id.r_img)
 
         val user = list.get(position)
 
         r_title.text = "${user.first} ${user.last}"
         r_email.text = user.email
         r_age.text = user.age.toString()
+
+        Glide.with(rootView).load(user.image).into(r_image)
 
         return rootView
     }

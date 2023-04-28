eac43a7da8f2e743996fd923ef457a5a9459c8fc
Hakan ÖZER <hakanozer02@gmail.com>
2023-04-28 21:49:31 +0300

    Days-8

--- Android_Apps/Days8/app/src/main/java/com/example/days_8/Result.kt
+++ Android_Apps/Days8/app/src/main/java/com/example/days_8/Result.kt
@@ -3,13 +3,30 @@ package com.example.days_8
 import android.util.Log
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 class Result {
 
-    fun news () {
+    fun news () : List<News>  {
+        val arr = mutableListOf<News>()
         val url = "https://www.haberler.com/"
         val doc:Document = Jsoup.connect(url).timeout(15000).get()
-        Log.d("Title" , doc.title() )
+        val elements:Elements = doc.getElementsByAttribute("data-headlinenumber")
+        for ( item in elements) {
+
+            //Log.d("item", item.getElementsByTag("img").toString() )
+            val img = item.getElementsByTag("img")
+
+            val href = item.attr("abs:href")
+            val src = img.attr("data-src")
+            val title = img.attr("alt")
+
+            if ( title != "" && href != "" && src != "" ) {
+                val news = News(title, src, href)
+                arr.add(news)
+            }
+        }
+        return arr
     }
 
 }

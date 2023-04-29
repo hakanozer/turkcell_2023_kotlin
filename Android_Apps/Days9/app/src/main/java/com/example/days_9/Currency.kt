2a24beebd03765474fecedf89075efaeade8cb13
Hakan ÖZER <hakanozer02@gmail.com>
2023-04-29 21:40:41 +0300

    Days-9

---
+++ Android_Apps/Days9/app/src/main/java/com/example/days_9/Currency.kt
@@ -0,0 +1,9 @@
+package com.example.days_9
+
+data class Currency(
+    val Isim:String,
+    val ForexBuying:String,
+    val ForexSelling:String,
+    val BanknoteBuying:String,
+    val BanknoteSelling:String,
+)

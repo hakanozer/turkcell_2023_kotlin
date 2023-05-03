a6ee03ae1a05dc7598863b1c755dd078e4b47c4f
gulten27 <ggdogann00@gmail.com>
2023-05-03 19:12:09 +0300

    commit

--- Android_Apps/Ödevler/5/Gulten_Dogan_Odev5/app/src/main/java/com/gultendogan/gulten_dogan_odev5/Currency.kt
+++
@@ -1,9 +0,0 @@
-package com.gultendogan.gulten_dogan_odev5
-
-data class Currency(
-    val Isim:String,
-    val ForexBuying:String,
-    val ForexSelling:String,
-    val BanknoteBuying:String,
-    val BanknoteSelling:String,
-)

9e228bef5a92d47460f366b847627afee45f5040
gulten27 <ggdogann00@gmail.com>
2023-05-03 19:12:09 +0300

    commit

--- Android_Apps/Ödevler/5/Gulten_Dogan_Odev5/app/src/main/java/com/gultendogan/gulten_dogan_odev5/PLANT.kt
+++
@@ -1,9 +0,0 @@
-package com.gultendogan.gulten_dogan_odev5
-
-data class PLANT(
-    val COMMON:String,
-    val BOTANICAL:String,
-    val ZONE:String,
-    val LIGHT:String,
-    val PRICE:String,
-)

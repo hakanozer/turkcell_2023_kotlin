07c9d42c42d78c24f09a0ebadcc525e90af742bf
Hakan ÖZER <hakanozer02@gmail.com>
2023-05-10 21:32:30 +0300

    Delete - Update

--- Android_Apps/Days13/app/src/main/java/com/example/days_13/DB.kt
+++ Android_Apps/Days13/app/src/main/java/com/example/days_13/DB.kt
@@ -2,8 +2,11 @@ package com.example.days_13
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import com.example.days_13.models.Note
 
 class DB(context: Context) : SQLiteOpenHelper(context, DBName, null, Version  ) {
 
@@ -40,6 +43,39 @@ class DB(context: Context) : SQLiteOpenHelper(context, DBName, null, Version  ) 
         val effectRow = db.insert("note", null, values)
         db.close()
         return effectRow
+    }
+
+    fun deleteNote(nid: Int) : Int {
+        val db = this.writableDatabase
+        val status = db.delete("note", "nid = $nid", null )
+        db.close()
+        return status
+    }
+
+    fun  updateNote(title:String, detail: String, nid: Int) : Int {
+        val db = this.writableDatabase
+        val content = ContentValues()
+        content.put("title", title)
+        content.put("detail", detail)
+        val status = db.update("note", content, "nid = $nid", null)
+        db.close()
+        return status
+    }
+
+    fun allNote() : List<Note> {
+        val db = this.readableDatabase
+        val arr = mutableListOf<Note>()
+        val cursor = db.query("note",null, null, null, null, null, null)
+        while (cursor.moveToNext()) {
+            val nid = cursor.getInt(0)
+            val title = cursor.getString(1)
+            val detail = cursor.getString(2)
+            val date = cursor.getString(3)
+            val note = Note(nid, title, detail, date)
+            arr.add(note)
+        }
+        db.close()
+        return arr
     }
 
 }

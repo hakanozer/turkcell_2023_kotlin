9efa3b3baf56afc3dea57833870758996721f3fe
Hakan ÖZER <hakanozer02@gmail.com>
2023-05-12 21:31:27 +0300

    Lifecycle Methods

---
+++ Android_Apps/Days14/app/src/main/java/com/example/days_14/Profile.kt
@@ -0,0 +1,11 @@
+package com.example.days_14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}

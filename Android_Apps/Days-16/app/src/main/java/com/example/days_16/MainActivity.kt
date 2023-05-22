a80074f6f1f7f7a7f5f82878c86a8ca8b9e2ff2a
Hakan ÖZER <hakanozer02@gmail.com>
2023-05-22 20:07:25 +0300

    Days-16

---
+++ Android_Apps/Days-16/app/src/main/java/com/example/days_16/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.days_16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btn1:Button
+    lateinit var btn2:Button
+    lateinit var btn3:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val one = One()
+        one.arguments?.putString("key1", "data1")
+        changeFragment(one)
+
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
+        btn3 = findViewById(R.id.btn3)
+        btn1.setOnClickListener {
+            one.arguments?.putString("key1", "data1")
+            changeFragment(one)
+        }
+        btn2.setOnClickListener {
+            val two = Two()
+            changeFragment(two)
+        }
+        btn3.setOnClickListener {
+            val three = Three()
+            changeFragment(three)
+        }
+
+    }
+
+
+    fun changeFragment ( fragment: Fragment ) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
+}

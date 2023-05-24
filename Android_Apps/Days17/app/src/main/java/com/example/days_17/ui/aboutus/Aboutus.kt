a44a43cb75d339b0e602f85635a4bb92887a864d
Hakan ÖZER <hakanozer02@gmail.com>
2023-05-24 21:50:18 +0300

    Days-17

--- Android_Apps/Days17/app/src/main/java/com/example/days_17/ui/aboutus/Aboutus.kt
+++ Android_Apps/Days17/app/src/main/java/com/example/days_17/ui/aboutus/Aboutus.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.days_17.R
+import com.example.days_17.databinding.FragmentAboutusBinding
+import com.example.days_17.databinding.FragmentGalleryBinding
+import com.example.days_17.ui.gallery.GalleryViewModel
 
 class Aboutus : Fragment() {
 
@@ -14,19 +18,35 @@ class Aboutus : Fragment() {
         fun newInstance() = Aboutus()
     }
 
+    private var _binding: FragmentAboutusBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
     private lateinit var viewModel: AboutusViewModel
+    private lateinit var text_aboutus: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_aboutus, container, false)
+        viewModel =
+            ViewModelProvider(this).get(AboutusViewModel::class.java)
+        _binding = FragmentAboutusBinding.inflate(inflater, container, false)
+        //val view = inflater.inflate(R.layout.fragment_aboutus, container, false)
+        val view = binding.root
+        text_aboutus = binding.textAboutus
+        viewModel.text.observe(viewLifecycleOwner) {
+            text_aboutus.setText(it)
+        }
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AboutusViewModel::class.java)
-        // TODO: Use the ViewModel
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
--- Android_Apps/Days17/app/src/main/java/com/example/days_17/ui/aboutus/AboutusViewModel.kt
+++ Android_Apps/Days17/app/src/main/java/com/example/days_17/ui/aboutus/AboutusViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.days_17.ui.aboutus
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AboutusViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _text = MutableLiveData<String>().apply {
+        value = "Hakkımızda Page"
+    }
+    val text: LiveData<String> = _text
 }

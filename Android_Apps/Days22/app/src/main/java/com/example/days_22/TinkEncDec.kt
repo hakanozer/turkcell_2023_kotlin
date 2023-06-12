1d310efb2d43d4fba182cb8218ff297b3630df83
Hakan ÖZER <hakanozer02@gmail.com>
2023-06-12 21:12:56 +0300

    Days-22

--- Android_Apps/Days22/app/src/main/java/com/example/days_22/TinkEncDec.kt
+++ Android_Apps/Days22/app/src/main/java/com/example/days_22/TinkEncDec.kt
@@ -3,7 +3,6 @@ package com.example.days_22
 import android.util.Log
 import com.google.crypto.tink.subtle.AesGcmJce
 import com.google.crypto.tink.subtle.Base64
-import java.nio.charset.Charset
 
 class TinkEncDec {
 
@@ -21,6 +20,20 @@ class TinkEncDec {
         }
         cipherText = Base64.encode(cipherText.toByteArray())
         return cipherText;
+    }
+
+    fun decrypt(cipherText: String): String {
+        var planText: String = ""
+        try {
+            val cipherText = String(Base64.decode(cipherText))
+            val convertEncode = cipherText.toByteArray(charset("ISO-8859-1"))
+            val aesGcmJce = AesGcmJce(key128Bit.toByteArray())
+            val descBytes = aesGcmJce.decrypt(convertEncode, extraKey.toByteArray())
+            planText = String(descBytes)
+        } catch (e: java.lang.Exception) {
+            System.err.println("decrypt Error : $e")
+        }
+        return planText
     }
 
 }
